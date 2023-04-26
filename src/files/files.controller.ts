3ab30c3276c213c03895d4b6e10bc233df1828cd
Alejandro Estrada <alejoestradam@yahoo.com>
2023-04-26 14:52:30 -0500

    file controller management and validations. FileSystem Save

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
    @Post('product')
-   @UseInterceptors(FileInterceptor('file'))
+   @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter,
+    //limits: {fileSize: 1000}
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
+   }))
    uploadProductImage(
       @UploadedFile() file: Express.Multer.File,
    ){
-    return file;
-   }
 
+    console.log("file in controller", file)
+
+    if( !file ) throw new BadRequestException('Make sure that file is an image');
+
+    return {
+      fileName: file.originalname,
+    }
+   }
 }
