d3fa2f7afdd5ce1fc20060032e90282cd4f3bf4f
Alejandro Estrada <alejoestradam@yahoo.com>
2023-05-05 10:12:12 -0500

    setting up jwt with process.env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,34 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       User
-    ])
+    ]),
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => {
+        console.log(process.env.JWT_SECRET)
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '24h'
+          }
+        } 
+      }
+    })
+    // JwtModule.register({ // it might be possible that the app does not have env variables in the moment the app is mounted, in this case, we use registerAsync 
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '24h'
+    //   }
+    // })
   ],
   controllers: [AuthController],
   providers: [AuthService],
