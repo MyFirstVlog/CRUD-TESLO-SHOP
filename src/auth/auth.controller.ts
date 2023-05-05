77511846d0bfbb583dc73060c93cc77334c4f141
Alejandro Estrada <alejoestradam@yahoo.com>
2023-05-05 09:59:15 -0500

    verify passsword with hash sync, creating login dto and changin user attr entity as not showable when select

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateuserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,11 @@ export class AuthController {
   @Post('register')
   create(@Body() createAuthDto: CreateuserDto) {
     return this.authService.create(createAuthDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateuserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/users.entity';
+import * as bcrypt from 'bcrypt'
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,15 +16,35 @@ export class AuthService {
   async create(createUserDto: CreateuserDto) {
     
     try {
-      const user = this.userRepository.create(createUserDto);
+
+      const {password, ...userData} = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
 
       await this.userRepository.save(user);
+      delete user.password;
 
       return user;
     } catch (error) {
       this.handleDBErrors(error);
     }
     
+  }
+
+  async login( loginUserDto: LoginUserDto){
+    const {email, password} = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: {email: true, password: true}
+    });
+
+    if(!user) throw new UnauthorizedException('Credentials are not valid (email)');
+    if(!bcrypt.compareSync(password, user.password)) throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
   }
 
   private handleDBErrors(error: any): never{ // never return value
