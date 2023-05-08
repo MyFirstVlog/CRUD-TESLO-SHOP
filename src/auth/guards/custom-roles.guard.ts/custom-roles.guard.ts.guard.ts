5dd68023dddf323f913ae840572f16789c6f391f
Alejandro Estrada <alejoestradam@yahoo.com>
2023-05-08 10:32:54 -0500

    [authorization] roles first strategy without using decorators

--- src/auth/guards/custom-roles.guard.ts/custom-roles.guard.ts.guard.ts
+++ src/auth/guards/custom-roles.guard.ts/custom-roles.guard.ts.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from '../../entities/users.entity';
 
 @Injectable()
 export class CustomRolesGuard implements CanActivate {
@@ -12,9 +13,15 @@ export class CustomRolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[]  = this.reflector.get('roles', context.getHandler());
+    const user = context.switchToHttp().getRequest().user as User; 
+    
+    if(validRoles.length === 0) return true;
+    if(!user) throw new NotFoundException('User not found');
 
-    console.log(this.reflector.get('roles', context.getHandler()));
-
-    return false;
+    for (const role of user.roles) {
+      if(validRoles.includes(role)) return true;
+    }
+    throw new ForbiddenException(`User ${user.fullName} need a valid role [${validRoles}]`);
   }
 }
